2244ce7514621e1f0716e268159eb143d691429a
SirRandoo <randomshovel@gmail.com>
2019-11-24 04:31:12 -0600

    Initial source commit.

---
+++ Source/Patches/Pawn_GetGizmos.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+using Harmony;
+
+using RimWorld;
+
+using UnityEngine;
+
+using Verse;
+
+namespace SirRandoo.WheresMyBed.Patches
+{
+    [HarmonyPatch(typeof(Pawn), "GetGizmos")]
+    public static class Pawn_GetGizmos__Postfix
+    {
+        [HarmonyPostfix]
+        public static void GetGizmos(Pawn __instance, ref IEnumerable<Gizmo> __result)
+        {
+            if(__instance != null && __instance.IsColonistPlayerControlled)
+            {
+                foreach(var map in Find.Maps)
+                {
+                    if(!map.IsPlayerHome)
+                        continue;
+
+                    foreach(var thing in map.listerThings.AllThings)
+                    {
+                        if(!(thing is Building_Bed))
+                            continue;
+                        var bed = (Building_Bed) thing;
+
+                        if(bed.owners.Contains(__instance))
+                        {
+                            __result = __result.Add(new Command_Action
+                            {
+                                defaultLabel = "WMB.Gizmo.Label".Translate(),
+                                defaultDesc = "WMB.Gizmo.Description".Translate(),
+                                icon = ContentFinder<Texture2D>.Get("UI/Icons/WMB_Gizmo"),
+                                activateSound = SoundDef.Named("Click"),
+                                action = delegate { CameraJumper.TryJumpAndSelect(bed); }
+                            });
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Source/WMB.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Reflection;
+
+using Harmony;
+
+using Verse;
+
+namespace SirRandoo.WheresMyBed
+{
+    public class WMB : Mod
+    {
+        internal static HarmonyInstance Harmony;
+
+        public WMB(ModContentPack content) : base(content)
+        {
+            Log.Message("Where's My Bed :: Have you seen my bed?");
+
+            Harmony = HarmonyInstance.Create("sirrandoo.wmb");
+            Harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
